8f72982833de85d6171f3f1f0614c152d2902c7a
fuzzylimes <fuzzylimes@gmail.com>
2025-04-29 13:31:46 -0400

    Initial commit

---
+++ test/reciprocal-merge-proxy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReciprocalMergeProxy from '../lib/reciprocal-merge-proxy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/reciprocal-merge-proxy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReciprocalMergeProxy.ReciprocalMergeProxyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
